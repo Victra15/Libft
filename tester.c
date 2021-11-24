50488bec6669bae377402e35d99259c0b084b8f8
Yongtae Lee <yolee@c8r6s4.42seoul.kr>
2021-11-24 18:51:48 +0900

    isprint

--- tester.c
+++ tester.c
@@ -6,14 +6,21 @@
 /*   By: yolee <yolee@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:59:49 by yolee             #+#    #+#             */
-/*   Updated: 2021/11/24 17:59:50 by yolee            ###   ########.fr       */
+/*   Updated: 2021/11/24 18:45:20 by yolee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include	"libft.h"
+#include	<stdio.h>
+#include	<ctype.h>
 
 int	main(void)
 {
-	
+	int	boo;
+
+	boo = isascii(127);
+	printf("%d", boo);
+	boo = isascii(128);
+	printf("%d", boo);
 	return (0);
 }
